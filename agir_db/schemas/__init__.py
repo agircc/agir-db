f7c2159d1d7a1d3323d3a8ab410c2e9e9cbd7d81
steven <steven.zg@outlook.com>
2025-04-25 10:47:28 +1200

    fix: import dto

--- agir_db/schemas/__init__.py
+++ agir_db/schemas/__init__.py
@@ -1,5 +1,5 @@
 from agir_db.schemas.auth import TokenPayload, Token, SendVerificationCode, VerifyEmail
-from agir_db.schemas.user import User, UserCreate, UserUpdate, UserBase
+from agir_db.schemas.user import UserDTO, UserCreate, UserUpdate, UserBase
 from agir_db.schemas.task import (
     Task, TaskCreate, TaskUpdate, TaskDetail, TaskList, TaskBrief,
     TaskComment, TaskCommentCreate, TaskCommentUpdate, TaskCommentList,
@@ -7,7 +7,6 @@ from agir_db.schemas.task import (
     TaskCountSummary
 )
 from agir_db.schemas.user_capability import UserCapabilityCreate, UserCapabilityUpdate
-from agir_db.schemas.memory import UserMemoryCreate, UserMemoryUpdate, UserMemoryResponse, UserMemoryList, MemorySearchParams
 from agir_db.schemas.models import ModelInfo, ModelsResponse, ModelCategory
 from agir_db.schemas.process_role_user import ProcessRoleUser, ProcessRoleUserCreate, ProcessRoleUserUpdate
 
