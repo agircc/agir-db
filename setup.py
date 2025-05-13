69100f4372d1ed01617937538c88faf09648abd1
steven <steven.zg@outlook.com>
2025-05-13 18:50:17 +1200

    fix: version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="agir_db",
-    version="0.1.0",
+    version="0.1.1",
     description="Database models and migration code for AGIR application",
     author="AGIR Team",
     author_email="info@agir.cc",
